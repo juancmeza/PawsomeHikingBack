9d0a903504e4ff83bc6314c62801627eca212b91
juan_carlos_meza <juancmezat@gmail.com>
2021-02-28 13:24:55 -0800

    finished JWT, needs testing

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,21 @@
 class Api::V1::UsersController < ApplicationController
 
-    def create
-        @user = User.create(user_params)
-        if @user.valid?
-          render json: { user: UserSerializer.new(@user) }, status: :created
-        else
-          render json: { error: 'failed to create user' }, status: :not_acceptable
-        end
-    end
+  skip_before_action :authorized, only: [:create]
 
-    private
 
-    def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :password)
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: 'failed to create user' }, status: :not_acceptable
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :phone_number, :password)
+  end
 end
