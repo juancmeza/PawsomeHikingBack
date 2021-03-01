84afc30be77f692717c0e9c092dfd98fd948578c
juan_carlos_meza <juancmezat@gmail.com>
2021-03-01 00:48:43 -0800

    working on fixing params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :phone_number)
+        params.require(:user).permit(:first_name, :last_name, :email, :phone_number, :username, :password)
     end
 end
