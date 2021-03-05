54f0a927ddc646c74b0ea3519027c1d1b156aec2
juan_carlos_meza <juancmezat@gmail.com>
2021-03-05 00:03:39 -0800

    create dog_trip

--- app/controllers/dog_trips_controller.rb
+++ app/controllers/dog_trips_controller.rb
@@ -1,4 +1,14 @@
 class DogTripsController < ApplicationController
     skip_before_action :authorized
 
+    def create
+      dog_trip = DogTrip.create(dog_trip_params)
+      render json: dog_trip
+    end
+
+    private
+
+    def dog_trip_params
+      params.require(:dog_trip).permit(:dog_id, :trip_id)
+    end    
 end
