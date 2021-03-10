4b99ce4e8ed01ab815c3675f15991258d6383517
juan_carlos_meza <juancmezat@gmail.com>
2021-03-09 18:12:27 -0800

    dog update

--- app/controllers/dog_trips_controller.rb
+++ app/controllers/dog_trips_controller.rb
@@ -2,12 +2,10 @@ class DogTripsController < ApplicationController
     skip_before_action :authorized
 
     def create
-      byebug
       params[:dogs].each do |dog|
         DogTrip.create(dog_id: dog[:id], trip_id: params[:trip_id])
       end
     render json: { user: UserSerializer.new(current_user) }
-
     end
 
     # private
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -12,10 +12,21 @@ class DogsController < ApplicationController
       render json: { user: UserSerializer.new(current_user) }
     end
 
+    def update
+      # dog = Dog.find_by(name: dog_params[:dog][:name, user_id: dog_params[:dog][:user_id]])
+      dog = Dog.find(dog_params[:id])
+      dog.update(dog_params)
+      render json: { user: UserSerializer.new(current_user) }
+
+      # user = User.find_by(id: params[:id])
+      # user.update(email: user_params[:email], username: user_params[:username], password: user_params[:password])
+      # render json: user
+    end
+
     private
 
     def dog_params
-      params.require(:dog).permit(:name, :weight, :breed, :age, :user_id)
+      params.require(:dog).permit(:name, :weight, :breed, :age, :user_id, :id)
     end 
 
 end
