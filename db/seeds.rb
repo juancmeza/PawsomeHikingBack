21b29b6b149847b0d9448303232d69f35e90641a
juan_carlos_meza <juancmezat@gmail.com>
2021-04-10 15:40:51 -0700

    Changed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,13 +16,17 @@ User.destroy_all
 johnsmith = User.create(username:"jimmy", first_name: "John", last_name: "Smith", email: "js@gmail.com", phone_number: "(888) 555-5555", password: "123")
 
 trip1 = Trip.create(date: Date.today, location: 'Fort Funston', time: (Time.now.utc).strftime("%I:%M %p"))
-trip2 = Trip.create(date: (Date.today + 2), location: 'Sigmund Stern Recreation Grove', time: (Time.now.utc).strftime("%I:%M %p"))
+trip2 = Trip.create(date: (Date.today + 2), location: 'Stern Grove', time: (Time.now.utc).strftime("%I:%M %p"))
 trip3 = Trip.create(date: (Date.today + 3), location: "Marshall's Beach", time: (Time.now.utc).strftime("%I:%M %p"))
 trip4 = Trip.create(date: (Date.today + 4), location: "Lands End", time: (Time.now.utc).strftime("%I:%M %p"))
 trip5 = Trip.create(date: (Date.today + 5), location: 'Fort Funston', time: (Time.now.utc).strftime("%I:%M %p"))
-trip6 = Trip.create(date: (Date.today + 6), location: 'Sigmund Stern Recreation Grove', time: (Time.now.utc).strftime("%I:%M %p"))
-trip7 = Trip.create(date: (Date.today + 8), location: "Marshall's Beach", time: (Time.now.utc).strftime("%I:%M %p"))
-trip8 = Trip.create(date: (Date.today + 9), location: "Lands End", time: (Time.now.utc).strftime("%I:%M %p"))
+trip6 = Trip.create(date: (Date.today + 6), location: 'Stern Grove', time: (Time.now.utc).strftime("%I:%M %p"))
+trip7 = Trip.create(date: (Date.today + 7), location: "Marshall's Beach", time: (Time.now.utc).strftime("%I:%M %p"))
+trip8 = Trip.create(date: (Date.today + 8), location: "Lands End", time: (Time.now.utc).strftime("%I:%M %p"))
+trip9 = Trip.create(date: (Date.today + 9), location: 'Fort Funston', time: (Time.now.utc).strftime("%I:%M %p"))
+trip10 = Trip.create(date: (Date.today + 10), location: 'Stern Grove', time: (Time.now.utc).strftime("%I:%M %p"))
+trip11 = Trip.create(date: (Date.today + 11), location: "Marshall's Beach", time: (Time.now.utc).strftime("%I:%M %p"))
+trip12 = Trip.create(date: (Date.today + 12), location: "Lands End", time: (Time.now.utc).strftime("%I:%M %p"))
 
 
 dog1 = Dog.create(name: 'Rocky', weight: '65', breed: 'Bracco Italiano', age: '2 years', user_id: johnsmith.id)
